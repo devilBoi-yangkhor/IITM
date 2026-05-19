f248e71c56e369228c90ff3b7f983a857e712fcb
devilBoi_YangKhor <ugyendendup@yangkhor.com>
2026-05-19 12:48:36 +0600

    19/05/2026

--- training_institute/settings.py
+++ training_institute/settings.py
@@ -122,10 +122,26 @@ WSGI_APPLICATION = 'training_institute.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#Local Server Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'training_institute_db',
+        'USER': 'training_user',
+        'PASSWORD': 'StrongPassword123!',   # ← Put the password you created
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
