a0c170b8afc05a2a956bd675fe1b6dee371a8a44
devilBoi_YangKhor <ugyendendup@yangkhor.com>
2026-05-19 12:37:30 +0600

    19/05/2026

--- training_institute/settings.py
+++ training_institute/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure--m4#9&k7a!l8-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1,172.30.20.17').split(',')
 
 AUTH_USER_MODEL = 'training_institute_app.User'
 
